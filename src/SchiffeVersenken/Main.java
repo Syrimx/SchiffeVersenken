53af649acbb52629f80ed7f0ccaa7c53f02af203
ElySchybol <rimx@gmx.net>
2023-06-27 21:38:10 +0200

    Loop hinzugefügt

--- src/SchiffeVersenken/Main.java
+++ src/SchiffeVersenken/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Don't Panic");
+		for(int i = 0; i <= 10 ; i++) {
+			System.out.println("Don't Panic");
+		}
 	}
 
 }
