cf849ce5ecb4ffff66ae4981272e9ab0ef35349b
ElySchybol <rimx@gmx.net>
2023-07-08 13:09:10 +0200

    DataModel Checkup

--- src/SchiffeVersenken/DataModel.java
+++ src/SchiffeVersenken/DataModel.java
@@ -16,11 +16,26 @@ public class DataModel {
     private char[][] playgroundMatrix    = null; //Wie groß darf sie werden ?                               //int durch char getauscht, um mehr möglichkeiten zu haben, wie Nutzung von X
     private int[] fireLocation          = null; //gewonnen durch GUI.getCurrentLocation
 
+
+    /* public DataMode() {
+        -> Intiale Matrix erstellen // also einmal beim Initialisierendes Objektes
+        // Groeße des Feldes nach klassischen Spielregeln 10 x 10 
+        this.playgroundMatrix = new char [10][10];
+
+            //Erzeugen eines 10 x 10 Feldes gefüllt mit Nullen zum testen des Arrays
+            for(int i = 0, i < playgroundMatrix.length; i++){
+                for(j = 0, j < playgroundMatrix[i].length; j++){
+                    System.out.print(playgroundMatrix[i][j] + " ");
+                }
+                System.out.println();           //geht eine Zeile nach unten
+            }
+    } */
+
     /* DataModel Methoden */
-    public int[][] getData() {
+    public char[][] getData() {
 
         // Groeße des Feldes nach klassischen Spielregeln 10 x 10 
-        private char[][] playgroundMatrix = new char [10][10];
+        this.playgroundMatrix = new char [10][10];
 
             //Erzeugen eines 10 x 10 Feldes gefüllt mit Nullen zum testen des Arrays
             for(int i = 0, i < playgroundMatrix.length; i++){
@@ -30,7 +45,7 @@ public class DataModel {
                 System.out.println();           //geht eine Zeile nach unten
             }
 
-        return playgroundMatrix;
+        return this.playgroundMatrix;
     }
 
     public void setData(char[][] data) {
