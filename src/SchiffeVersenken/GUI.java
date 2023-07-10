4e4871d26219f10a97e7475bbb6443b13ff07994
0katana <ronjakircheis@gmail.com>
2023-07-10 13:06:42 +0200

    Änderung an GUI klasse

--- src/SchiffeVersenken/GUI.java
+++ src/SchiffeVersenken/GUI.java
@@ -1,9 +1,17 @@
 package SchiffeVersenken;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 /* View
  * |-> bezieht Matrix von Model
@@ -22,8 +30,13 @@ import javax.swing.JFrame;
 public class GUI extends JFrame
 {
     private MenuWindow menuWindow;
-    private JButton backtoMenuButton;
-    /* Eigenschaften */
+    private JPanel instructionPanel;
+    private JPanel playerPanel;
+    private JPanel enemyPanel;
+    private JLabel instructionLabel;
+    private JMenuBar menubar;
+    private JMenu menuGame;
+    private JMenuItem backtoMenu;
     private DataModel model;
     
     public GUI(MenuWindow menuWindow) {
@@ -31,16 +44,40 @@ public class GUI extends JFrame
     	//Datenmodel in View eingebunden, sodass Daten einfacher aufrufbar sind
         this.model = new DataModel();
     	
-    	this.setLayout(null);
+    	this.setLayout(new BorderLayout());
     	this.setTitle("Schiffe versenken");
 		this.getContentPane().setBackground(Color.LIGHT_GRAY);
 		this.setLocation(200, 200);
 		this.setSize(800, 600); 
 		
-    	backtoMenuButton = new JButton("Zurueck zum Menue");
-    	backtoMenuButton.addActionListener(e -> menuWindow.backToMenu());
-    	backtoMenuButton.setBounds(100, 5, 110, 30);
-    	this.add(backtoMenuButton);
+		instructionPanel = new JPanel();
+		instructionPanel.setPreferredSize(new Dimension(800,50));
+		instructionPanel.setBackground(Color.LIGHT_GRAY);
+		add(instructionPanel,BorderLayout.NORTH);
+		
+		enemyPanel = new JPanel(new GridLayout(10,10));
+		enemyPanel.setPreferredSize(new Dimension(400,550)); //Mindestgroess für playerPanel
+		enemyPanel.setBackground(Color.DARK_GRAY);
+		add(enemyPanel, BorderLayout.WEST);
+		
+		playerPanel = new JPanel(new GridLayout(10,10));
+		playerPanel.setPreferredSize(new Dimension(400,550));
+		playerPanel.setBackground(Color.WHITE);
+		add(playerPanel, BorderLayout.EAST);
+		
+		menubar = new JMenuBar();
+		menuGame = new JMenu("Menü");
+		backtoMenu = new JMenuItem("Spiel beenden");
+		menubar.add(menuGame);
+		menuGame.add(backtoMenu);
+		this.setJMenuBar(menubar);
+		backtoMenu.addActionListener(e -> menuWindow.backToMenu());
+		
+		instructionLabel = new JLabel("Platziere deine Schiffe!");
+		instructionLabel.setBounds(200, 5, 300, 40);
+		instructionPanel.add(instructionLabel);
+    	
+    	
     	setVisible(true);
     }
     
