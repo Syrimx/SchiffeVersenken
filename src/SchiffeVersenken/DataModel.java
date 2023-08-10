86f82aba174ab5891441b3fce76cda3c67384e97
0katana <ronjakircheis@gmail.com>
2023-08-10 17:19:20 +0200

    DataModel: ShipStatus setzen Funktion

--- src/SchiffeVersenken/DataModel.java
+++ src/SchiffeVersenken/DataModel.java
@@ -97,6 +97,7 @@ public class DataModel {
         //Diagonalität vermeiden
         while(startX == endX || startY == endY){
             System.out.println("Dieses Feld ist grün");
+            setShipStatus(startX,startY,endX,endY);
         }
 
         while(startX != endX || startY != endY){ 
@@ -107,24 +108,28 @@ public class DataModel {
         //Bedingungen für setzen der Schiffe 
         if(endX - startX == 1 || startX - endX == -1 || endY - startY == 1 || startY - endY == -1){
             submarine--;
+            setShipStatus(startX,startY,endX,endY);
             if(submarine <= 0){
                 System.out.print("Du hast keine U-Boote mehr");
             }else{}
 
         }else if(endX - startX == 2 || startX - endX == -2 || endY - startY == 2 || startY - endY == -2){
             frigate--;
+            setShipStatus(startX,startY,endX,endY);
             if(frigate <= 0){
                 System.out.print("Du hast keine Frigatten mehr");
             }else{}
 
         }else if (endX - startX == 3 || startX - endX == -3 || endY - startY == 3 || startY - endY == -3){
             cruiser--;
+            setShipStatus(startX,startY,endX,endY);
             if(cruiser <= 0){
                 System.out.print("Du hast keine Kreuzer mehr");
             }else{}
 
         }else if(endX - startX == 4 || startX - endX == -4 || endY - startY == 4 || startY - endY == -4){
             battleship--;
+            setShipStatus(startX,startY,endX,endY);
             if(battleship <= 0){
                 System.out.print("Du hast keine Schlachtschiffe mehr");
             }else{}
@@ -135,6 +140,23 @@ public class DataModel {
 
 
      }
+    
+    
+    //Status beim Schiffe setzen aktualisieren
+    private void setShipStatus(int startX,int startY,int endX,int endY) {
+	 // Hier wird mit der .compare Methode die Differenzen zwischen endX und startX (auch für y) berechnet (wichtig für die Richtung der Schleife)
+	    int dx = Integer.compare(endX, startX);
+	    int dy = Integer.compare(endY, startY);
+
+
+	    // verschachtelte schleifen durchlaufen Reihe (x) und Spalten (y) und setzt zwischen StartX und Endx (auch y) sowie jeweils den zwischenraum dx (bzw dy) ein "s"
+	    for (int x = startX; x != endX + dx; x += dx) {
+	        for (int y = startY; y != endY + dy; y += dy) {
+	            playerMatrix[x][y] = 's'; 
+	        }
+	    } 
+    }
+    
 
     // Methode zum Schießen auf gegnerisches Feld in Phase 2
     public void shootEnemyField(int row, int col) {
