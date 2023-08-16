b44b1548fc9dc35c0b594cc4915bd10a66457709
0katana <ronjakircheis@gmail.com>
2023-08-16 19:21:56 +0200

    Main unnötige Kommentare entfernt

--- src/SchiffeVersenken/Main.java
+++ src/SchiffeVersenken/Main.java
@@ -18,14 +18,6 @@ public class Main {
 		GUI gameWindow = new GUI();
 		gameWindow.drawMenu();
 		
-		//noch nicht ideal ausgelagert aus GUI 
-//		if(gameWindow.getGameBotOrFriend()) {
-//			
-//			gameWindow.resetGUI();
-//			gameWindow.drawMap();
-//			gameWindow.setInstruction("Test");
-//		
-//		}
 	}	
 
 }
